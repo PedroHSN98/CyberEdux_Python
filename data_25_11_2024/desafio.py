10c82e895cbd71acaa642a1f6ddfb042013c7577
Pedro Henrique Santana Nascimento <100177169+PedroHSN98@users.noreply.github.com>
2024-11-25 15:27:11 -0400

    desafio

---
+++ data_25_11_2024/desafio.py
@@ -0,0 +1,16 @@
+#Faça um programa de lista de nomes.
+#Nesse programa, o usuario deve informar 10 nomes,
+#os nomes devem ser inseridos em uma lista, e, em seguida, 
+#o programa deve mostrar os 10 nomes inseridos.
+nomes = []
+
+
+for i in range(10):
+    lista_nomes = str(input('Coloque o 1° Nome: '))
+    nomes.append(lista_nomes)
+    print(nomes)
+
+for p in range(10):
+    print(nomes[p])
+
+
