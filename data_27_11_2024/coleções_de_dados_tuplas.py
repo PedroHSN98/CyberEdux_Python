f5526cfbd1cd13d95600c048e39d53c41fce7b35
Pedro Henrique Santana Nascimento <100177169+PedroHSN98@users.noreply.github.com>
2024-11-27 15:25:24 -0400

    tupla

---
+++ data_27_11_2024/coleções_de_dados_tuplas.py
@@ -0,0 +1,18 @@
+#Coleções de dados - Tuplas
+
+#É uma coleção de dados sequencial, assim como a lista.
+#Sintaxe: minha_tupla('a', 'b', 'c')
+#                  ou 'a', 'b', 'c'
+#Tupla é extatica
+#Tupla é Imutável 
+
+minha_lista = ['a', 'b', 'c']
+minha_tupla = ('a', 'b', 'c')
+
+
+minha_lista[1] = 'B'
+#minha_lista[1] = 'B' / aqui vc mudou o resultado do 1
+print(minha_lista)
+
+#minha_tupa[1] = 'B'
+#print('minha_tupla')    / nisso ira dar erro, pois a tupla é Imutável
