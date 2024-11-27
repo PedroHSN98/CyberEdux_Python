cdc627c8e275b721f5b00761fddc343aa00ca761
Pedro Henrique Santana Nascimento <100177169+PedroHSN98@users.noreply.github.com>
2024-11-27 14:36:43 -0400

    Exemplo 2

---
+++ data_25_11_2024/desafio3-2.py
@@ -0,0 +1,22 @@
+nomes = []
+telefones = []
+
+n = int(input('Quantos telefones deseja inserir? R.: '))
+for i in range(n):
+    nome = input(f'{i+1}° nome: ')
+    telefone = input(f'{i+1}° telefone: ')
+    nomes.append(nome)
+    telefones.append(telefone)
+
+while True:
+    nome = input('Informe o nome da pessoa da qual você quer ver o telefone: ')
+    try:
+        indice = nomes.index(nome)
+        telefone = telefone[indice]
+        print(f'O telefone de {nome} é {telefone}')
+    except ValueError as ex:
+        print('Deu ruim!')
+
+
+
+# Try e Except, que foram inseridos aqui nesse codigo, um outro jeito de fazer o codigo do codigo anterior
