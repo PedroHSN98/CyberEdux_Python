e3047ca5c7f92fd7f635c517c80795ecb53d3c46
Pedro Henrique Santana Nascimento <100177169+PedroHSN98@users.noreply.github.com>
2024-12-04 16:38:17 -0400

    comitado

---
+++ data_04_12_2024/dicionario.py
@@ -0,0 +1,9 @@
+pessoas = {
+    'fulano@gmail.com' : 'Fulano',
+    'ciclano@gmail.com' : 'Ciclano',
+    'beltrano@gmail.com' : 'Beltrano'
+}
+
+email = 'ciclano@gmail.com'
+nome = pessoas[email]
+print(nome)
