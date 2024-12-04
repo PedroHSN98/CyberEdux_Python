69cb94c3095ab7c5f73c6f1b3e9b72fa99653b33
Pedro Henrique Santana Nascimento <100177169+PedroHSN98@users.noreply.github.com>
2024-12-04 08:04:16 -0400

    ,

--- data_28_10_2024/hello.py
+++ data_28_10_2024/hello.py
@@ -1,3 +1,10 @@
 a = float(input('Digite o valor a: ')) # Obtem o valor A
 b = float(input('Digite o valor b: ')) # Obtem o valor B
 print(f'O resultado {a} + {b} é {a + b}') # Calcula o resultado
+
+
+# Float - Seria Número com ponto flutuante / ex=0.5, 3.23, 1.11112, -3.1, …
+
+#Input = o usuario ira escrever alguma coisa na tela.
+
+#
