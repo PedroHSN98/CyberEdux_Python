4bf23ad917d5f30bb042d6e4ed8f126364ad7180
Pedro Henrique Santana Nascimento <100177169+PedroHSN98@users.noreply.github.com>
2025-01-13 14:29:20 -0400

    retorno das aulas

---
+++ data_13_01_2025_Resumo/ex1.py
@@ -0,0 +1,23 @@
+#Conteudo basico de Python
+
+# - Entrada e sáida
+    #print
+    #input
+
+# - Variavel 
+
+# - Tipos de dados
+    #int
+    #str
+    #float
+    #bool
+
+# - Estrutura de controle
+    #if - else
+    # while
+    # for
+
+# - Coleção de dados
+    #Lista
+    #Tupla
+    #Dicionario
