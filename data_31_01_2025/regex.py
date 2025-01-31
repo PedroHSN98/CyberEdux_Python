8140a7589a6057c6b64e80854544422a75ede81d
Pedro Henrique Santana Nascimento <100177169+PedroHSN98@users.noreply.github.com>
2025-01-31 16:05:38 -0400

    regex

---
+++ data_31_01_2025/regex.py
@@ -0,0 +1,7 @@
+import re
+
+# Click on Edit and place your email ID to validate
+email = "my.ownsite@our-earth.org"
+valid = re.match(r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$', email)
+
+print("Valid email address." if valid else "Invalid email address.")
