aa1f94d40b0d785b1aba0a168a6c63a02070f63d
Pedro Henrique Santana Nascimento <pedrohenriquesn90@hotmail.com>
2025-02-01 00:30:01 -0400

    validador

--- data_31_01_2025/regex.py
+++ data_31_01_2025/regex.py
@@ -1,7 +1,28 @@
 import re
 
-# Click on Edit and place your email ID to validate
-email = "my.ownsite@our-earth.org"
-valid = re.match(r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$', email)
+def validar_email(email):
+    email_pattern = r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$'
+    return re.match(email_pattern, email) is not None
 
-print("Valid email address." if valid else "Invalid email address.")
+def validar_cpf(cpf):
+    cpf_pattern = r'^\d{3}\.\d{3}\.\d{3}-\d{2}$'
+    return re.match(cpf_pattern, cpf) is not None
+
+# Solicita entrada do usuário
+email = input("Digite seu e-mail: ")
+cpf = input("Digite seu CPF (formato: 000.000.000-00): ")
+
+# Validação
+email_valido = validar_email(email)
+cpf_valido = validar_cpf(cpf)
+
+# Exibe resultados
+if email_valido:
+    print("E-mail válido.")
+else:
+    print("E-mail inválido.")
+
+if cpf_valido:
+    print("CPF válido.")
+else:
+    print("CPF inválido.")
