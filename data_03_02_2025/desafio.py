d415d1050e4152b45694a89250d007ff0636df1e
Pedro Henrique Santana Nascimento <100177169+PedroHSN98@users.noreply.github.com>
2025-02-05 14:49:50 -0400

    att

--- data_03_02_2025/desafio.py
+++ data_03_02_2025/desafio.py
@@ -4,30 +4,27 @@ Neste arquivo, os dados da seguinte tabela devem ser
 inseridos: 
 '''
 
-
-'''
-Faça um programa para criar um arquivo do Excel.
-Neste arquivo, os dados da seguinte tabela devem ser 
-inseridos: 
-'''
-
 import openpyxl
+# import openpyxl as xl
 
 tabela = [
     ['Fulano', 'fulano@gmail.com'],
     ['Ciclano', 'ciclano@gmail.com'],
     ['Beltrano', 'beltrano@gmail.com']
 ]
-
+# ai iria ficar xl.Workbook()
 wb = openpyxl.Workbook()
 ws = wb.active
+
 
 ws.append(['Nome', 'Email'])
 
 for linha in tabela:
     ws.append(linha)
 
-#Salvar
+# Salvar o arquivo
 wb.save('tabela.xlsx')
 
 print("Arquivo Excel criado com sucesso!")
+
+
